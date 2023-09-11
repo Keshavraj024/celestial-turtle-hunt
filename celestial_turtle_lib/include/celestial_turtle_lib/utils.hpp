4e5f064f90d95c90343f0808f57ff14b3a072547
keshav <keshavraj024@gmail.com>
2023-09-11 20:37:29 +0200

    add docstring

--- celestial_turtle_lib/include/celestial_turtle_lib/utils.hpp
+++ celestial_turtle_lib/include/celestial_turtle_lib/utils.hpp
@@ -7,8 +7,19 @@
 #include "turtlesim/srv/set_pen.hpp"
 #include "turtlesim/srv/kill.hpp"
 
+/**
+ * @namespace celestial_turtle_lib
+ * @brief Contains utility functions for working with celestial turtles.
+ */
 namespace celestial_turtle_lib
 {
+    /**
+     * @brief Generates a random number between the given range [min, max].
+     * @tparam T The type of number to generate (e.g., int, double).
+     * @param min The minimum value of the range.
+     * @param max The maximum value of the range.
+     * @return A random number between min and max.
+     */
     template <typename T>
     T GenerateRandomNumber(T min, T max)
     {
@@ -25,8 +36,19 @@ namespace celestial_turtle_lib
         return randomNum;
     }
 
+    /**
+     * @brief Turns off the trail line for a specified turtle.
+     * @param node A pointer to the ROS2 node.
+     * @param turtleName The name of the turtle to turn off the trail line for.
+     */
     void offTrailLine(rclcpp::Node *node, const std::string &turtleName);
-    void killTurtle(rclcpp::Node *node, const std::string &turlteName);
+
+    /**
+     * @brief Kills a specified turtle.
+     * @param node A pointer to the ROS2 node.
+     * @param turlteName The name of the turtle to kill.
+     */
+    void killTurtle(rclcpp::Node *node, const std::string &turtleName);
 
 } /* namespace celestial_turtle_lib */
 
