355909a134efa41716a7ec705fd7e180b4f676d4
keshav <keshavraj024@gmail.com>
2023-09-11 20:12:05 +0200

    add docstring

--- celestial_turtle_spawner/include/celestial_turtle_spawner/turtle_spawner.hpp
+++ celestial_turtle_spawner/include/celestial_turtle_spawner/turtle_spawner.hpp
@@ -1,7 +1,8 @@
 /**
  * @file celestial_turtle_spawner/turtle_spawner.hpp
- * @brief Spawns a turtle
+ * @brief This file defines the TurtleSpawner class, which spawns turtles and manages their lifecycle.
  */
+
 #ifndef CELESTIAL_TURTLE_SPAWNER__TURTLE_SPAWNER_HPP_
 #define CELESTIAL_TURTLE_SPAWNER__TURTLE_SPAWNER_HPP_
 
@@ -15,37 +16,58 @@
 
 namespace celestial_turtle_spawner
 {
+    /**
+     * @brief The TurtleSpawner class spawns turtles and manages their lifecycle.
+     */
     class TurtleSpawner : public rclcpp::Node
     {
     public:
+        /**
+         * @brief Constructs a TurtleSpawner instance.
+         */
         TurtleSpawner();
 
     private:
-        void callbackKillNode(const std_msgs::msg::Bool::SharedPtr shouldTerminate);
-        void TimerCallBack();
         /**
-         * @brief Spawns a new turtle at a random pose
+         * @brief Callback to handle turtle termination requests.
+         * @param shouldTerminate A boolean message indicating whether to terminate turtles.
+         */
+        void callbackKillNode(const std_msgs::msg::Bool::SharedPtr shouldTerminate);
+
+        /**
+         * @brief Timer callback for periodic turtle spawning.
+         */
+        void TimerCallBack();
+
+        /**
+         * @brief Spawns a new turtle at a random pose.
          */
         void SpawnTurtle();
 
         /**
-         * Publishes the information of the alive turtles
+         * @brief Publishes the information of the alive turtles.
          */
         void PublishAliveTurtles();
 
+        /**
+         * @brief Callback to handle turtle termination service requests.
+         * @param request The request to terminate a turtle.
+         * @param response The response indicating the result of the termination request.
+         */
         void killTurtleCallback(const celestial_turtle_interface::srv::KillTurtle::Request::SharedPtr &request,
-                                 const celestial_turtle_interface::srv::KillTurtle::Response::SharedPtr &response);
+                                const celestial_turtle_interface::srv::KillTurtle::Response::SharedPtr &response);
 
         std::vector<std::thread> m_killThreads;
-        rclcpp::TimerBase::SharedPtr m_timer;  
-        rclcpp::Service<celestial_turtle_interface::srv::KillTurtle>::SharedPtr m_killTurtleServer;                                                        /**< Calls the spawn turtle service*/
-        std::vector<std::thread> m_spawnThreads;                                                       /**< Threads calling spawn turtle service*/
-        std::vector<celestial_turtle_interface::msg::Turtle> m_aliveTurtles;                           /**< List of alive turtles*/
-        rclcpp::Publisher<celestial_turtle_interface::msg::Turtles>::SharedPtr m_aliveTurtlePublisher; /**< Publishes the alive turtles information*/
+        rclcpp::TimerBase::SharedPtr m_timer;
+        rclcpp::Service<celestial_turtle_interface::srv::KillTurtle>::SharedPtr m_killTurtleServer;
+        std::vector<std::thread> m_spawnThreads;
+        std::vector<celestial_turtle_interface::msg::Turtle> m_turtleSpawned;
+        rclcpp::Publisher<celestial_turtle_interface::msg::Turtles>::SharedPtr m_aliveTurtlePublisher;
         std::size_t m_maxTurtles{1};
-        celestial_turtle_interface::msg::Turtles m_aliveTurtle;
+        celestial_turtle_interface::msg::Turtles m_aliveTurtles;
         rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr m_stopNodeSubscriber;
     };
 
 } /* namespace celestial_turtle_spawner */
+
 #endif /* CELESTIAL_TURTLE_SPAWNER__TURTLE_SPAWNER_HPP_ */
--- celestial_turtle_spawner/src/turtle_spawner.cpp
+++ celestial_turtle_spawner/src/turtle_spawner.cpp
@@ -9,7 +9,7 @@ namespace celestial_turtle_spawner
         m_timer = this->create_wall_timer(std::chrono::seconds(3), std::bind(&TurtleSpawner::TimerCallBack, this));
         m_aliveTurtlePublisher = this->create_publisher<celestial_turtle_interface::msg::Turtles>("alive_turtles", 10);
         m_killTurtleServer = this->create_service<celestial_turtle_interface::srv::KillTurtle>("kill_turtle",
-                                                                                                std::bind(&TurtleSpawner::killTurtleCallback, this, std::placeholders::_1, std::placeholders::_2));
+                                                                                               std::bind(&TurtleSpawner::killTurtleCallback, this, std::placeholders::_1, std::placeholders::_2));
         m_stopNodeSubscriber = this->create_subscription<std_msgs::msg::Bool>("kill_nodes",
                                                                               10, std::bind(&TurtleSpawner::callbackKillNode, this, std::placeholders::_1));
     }
@@ -24,16 +24,23 @@ namespace celestial_turtle_spawner
     void TurtleSpawner::killTurtleCallback(const celestial_turtle_interface::srv::KillTurtle::Request::SharedPtr &request,
                                            const celestial_turtle_interface::srv::KillTurtle::Response::SharedPtr &response)
     {
+        if (request->name.empty())
+        {
+            response->success = false;
+        }
         m_killThreads.push_back(std::thread(celestial_turtle_lib::killTurtle, this, request->name));
-        response->success = true;
-        auto it = std::find_if(m_aliveTurtles.begin(), m_aliveTurtles.end(), [&request](const celestial_turtle_interface::msg::Turtle &turtle)
+        auto it = std::find_if(m_turtleSpawned.begin(), m_turtleSpawned.end(), [&request](const celestial_turtle_interface::msg::Turtle &turtle)
                                { return turtle.name == request->name; });
 
-        // If the element is found, erase it from the vector
-        if (it != m_aliveTurtles.end())
+        if (it != m_turtleSpawned.end())
         {
-            m_aliveTurtles.erase(it);
+            m_turtleSpawned.erase(it);
             PublishAliveTurtles();
+            response->success = true;
+        }
+        else
+        {
+            response->success = false;
         }
     }
 
@@ -44,7 +51,7 @@ namespace celestial_turtle_spawner
 
     void TurtleSpawner::SpawnTurtle()
     {
-        if (m_aliveTurtles.size() >= m_maxTurtles)
+        if (m_turtleSpawned.size() >= m_maxTurtles)
         {
             return;
         }
@@ -77,7 +84,7 @@ namespace celestial_turtle_spawner
             turtle.y = request->y;
             turtle.theta = request->theta;
             turtle.name = response->name;
-            m_aliveTurtles.push_back(turtle);
+            m_turtleSpawned.push_back(turtle);
             celestial_turtle_lib::offTrailLine(this, turtle.name);
             PublishAliveTurtles();
         }
@@ -89,8 +96,8 @@ namespace celestial_turtle_spawner
 
     void TurtleSpawner::PublishAliveTurtles()
     {
-        m_aliveTurtle.turtles = m_aliveTurtles;
-        m_aliveTurtlePublisher->publish(m_aliveTurtle);
+        m_aliveTurtles.turtles = m_turtleSpawned;
+        m_aliveTurtlePublisher->publish(m_aliveTurtles);
     }
 
 }
